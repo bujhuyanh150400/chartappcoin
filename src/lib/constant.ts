364f0bb73cd973ce3f27237b3064c6b347afee0a
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-15 15:02:50 +0700

    AnhBH - fix

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,6 @@
+//167.86.71.162
+//192.168.1.156
+export const WS_URL = 'ws://167.86.71.162:5001'; // Thay bằng IP backend thật
 
-export const WS_URL = 'wss://ws.vjlink.com.vn'; // Thay bằng IP backend thật
-
-export const BACKEND_API_URL = 'https://vjlink.com.vn/api'
+export const BACKEND_API_URL = 'http://167.86.71.162:8000/api'
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['app.vjlink.com.vn']
+    allowedHosts: ['167.86.71.162']
   },
 })
