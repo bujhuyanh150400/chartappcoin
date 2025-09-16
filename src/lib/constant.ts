461f323d973fcab67218ad5a4e53e0f152178c75
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-16 22:16:49 +0700

    AnhBH - fix

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,5 +2,5 @@
 //192.168.1.156
 export const WS_URL = 'ws://167.86.71.162:5001'; // Thay bằng IP backend thật
 
-export const BACKEND_API_URL = 'http://167.86.71.162:8000/api'
+export const BACKEND_API_URL = 'http://167.86.71.162/api'
 
