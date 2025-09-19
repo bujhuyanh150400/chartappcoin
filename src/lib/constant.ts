04d8a0b2a394473d6b1fab4b701a246882b44df7
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-19 18:09:08 +0700

    AnhBH - fx

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,9 @@
 //167.86.71.162
 //192.168.1.156
-export const WS_URL = 'ws://167.86.71.162:5001'; // Thay bằng IP backend thật
+//192.168.100.64
+// export const WS_URL = 'ws://167.86.71.162:5001';
+export const WS_URL = 'wss://ws.vjlink.com.vn';
 
-export const BACKEND_API_URL = 'http://167.86.71.162/api'
+// export const BACKEND_API_URL = 'http://167.86.71.162/api'
+export const BACKEND_API_URL = 'https://vjlink.com.vn/api'
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// 192.168.100.64
+// 167.86.71.162
+// app.vjlink.com.vn
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['167.86.71.162']
+    allowedHosts: ['app.vjlink.com.vn']
   },
 })
