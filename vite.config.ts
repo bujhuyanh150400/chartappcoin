afb5437d8790523a0310fb484ea291b5cb805220
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-23 05:19:20 +0700

    AnhBH - fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// 192.168.100.64
+// 103.81.84.63
 // 167.86.71.162
 // app.vjlink.com.vn
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['app.vjlink.com.vn']
+    allowedHosts: ['103.81.84.63']
   },
 })
